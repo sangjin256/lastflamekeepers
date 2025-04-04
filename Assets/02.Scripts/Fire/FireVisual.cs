6204c363ffde33159fda30752d543d7b0999d8af
김나현 <skgus917@gmail.com>
2025-04-04 12:55:54 +0900

    FireManager, FireVisual, FireRangeController 추가
    [Object]
    Fire
    FireLight
    - Light2D

---
+++ Assets/02.Scripts/Fire/FireVisual.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class FireVisual : MonoBehaviour
+{
+    private CircleCollider2D fireLightCollider;
+    private Light2D fireLight;
+
+    [SerializeField] private float lightRadiusMultiplier = 1f; // 불 범위 대비 라이트 크기 계수
+
+    private void Start()
+    {
+        fireLightCollider = GetComponent<CircleCollider2D>();
+        fireLight = GetComponentInChildren<Light2D>();
+
+        FireManager.Instance.SubscribeToRangeChange(OnFireRangeChanged);
+    }
+
+    private void OnFireRangeChanged(float newRange)
+    {
+        // 반지름을 크기로 사용 (적절히 스케일 조절 계수 곱해도 됨)
+        transform.localScale = Vector3.one * newRange;
+
+        // Collider 갱신
+        if (fireLightCollider != null)
+        {
+            fireLightCollider.radius = newRange;
+        }
+
+        // 라이트 범위
+        if (fireLight != null)
+        {
+            fireLight.pointLightOuterRadius = newRange * lightRadiusMultiplier;
+        }
+    }
+}
