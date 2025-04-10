164f5ea5446352cff3bb6b7b807fab4ce591372d
SangJin Lee <56784814+sangjin256@users.noreply.github.com>
2025-04-10 17:49:06 +0900

    [LSJ] 쉐이더 테스트중

--- Assets/02.Scripts/Unit/Unit.cs
+++ Assets/02.Scripts/Unit/Unit.cs
@@ -44,7 +44,8 @@ public class Unit : AInteractableEntity
         _interactType = InteractType.Unit;
         _navMeshAgent.speed = _unitStat.MoveSpeed.Value / 10f;
         Health = _unitStat.MaxHealth.Value;
-        _navMeshAgent.ResetPath();
+        _navMeshAgent.enabled = false;
+        _navMeshAgent.enabled = true;
     }
     private void Update()
     {
@@ -144,6 +145,7 @@ public class Unit : AInteractableEntity
         {
             return;
         }
+       
 
         Vector2 randomPoint = point + UnityEngine.Random.insideUnitCircle / 2;
         _navMeshAgent.SetDestination(randomPoint);
