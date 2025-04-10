27d55e4afb3941117febc914d08aac94c876c586
sangjin lee <sangjin256@naver.com>
2025-04-10 21:32:40 +0900

    [LSJ] 툴매니저 변경

--- Assets/02.Scripts/Tool/ToolManager.cs
+++ Assets/02.Scripts/Tool/ToolManager.cs
@@ -17,7 +17,6 @@ public class ToolManager : BehaviourSingleton<ToolManager>
 
     public Action OnToolCountChanged;
 
-
     private AnimationClip[,,] _toolAnimationClip = new AnimationClip[4, 4, 2];
     public AnimationClip[,,] ToolAnimationClip => _toolAnimationClip;
 
@@ -33,8 +32,10 @@ public class ToolManager : BehaviourSingleton<ToolManager>
     public LayerMask Clilckable;
 
     public GameObject[] ToolImagePrefab;
-   
+
     public GameObject handOverToolImage;
+
+    private List<ToolData> _toolDataList = new List<ToolData>();
     private void Awake()
     {
         Global.Instance.OnDataLoaded += _LoadTool;
@@ -87,22 +88,27 @@ public class ToolManager : BehaviourSingleton<ToolManager>
         //∏«º’
         ToolData data = DataTable.Instance.GetToolData(10000);
         _toolList.Add(new NoneTool(data.ToolType, data.ToolName, 0, 0));
+        _toolDataList.Add(data);
         _maxToolCountList.Add(999);
 
         data = DataTable.Instance.GetToolData(10001);
         _toolList.Add(new SwordTool(data.ToolType, data.ToolName, 0, data.Value));
+        _toolDataList.Add(data);
         _maxToolCountList.Add(data.MaxCount);
 
         data = DataTable.Instance.GetToolData(10002);
         _toolList.Add(new AxeTool(data.ToolType, data.ToolName, 0, data.Value));
+        _toolDataList.Add(data);
         _maxToolCountList.Add(data.MaxCount);
 
         data = DataTable.Instance.GetToolData(10003);
         _toolList.Add(new PickAxeTool(data.ToolType, data.ToolName, 0, data.Value));
+        _toolDataList.Add(data);
         _maxToolCountList.Add(data.MaxCount);
 
         data = DataTable.Instance.GetToolData(10004);
         _toolList.Add(new MedicineTool(data.ToolType, data.ToolName, 0, data.Value));
+        _toolDataList.Add(data);
         _maxToolCountList.Add(data.MaxCount);
 
         _currentToolCountList = new List<int>(new int[5]);
@@ -143,14 +149,36 @@ public class ToolManager : BehaviourSingleton<ToolManager>
         return true;
     }
 
+    public bool CheckCanUpgrade(ToolType type)
+    {
+        return GetTool(type).UpgradeLevel < ToolManager.Instance.MaxUpgradeLevel;
+    }
+
+    public int GetWoodCountToUpgrade(ToolType type)
+    {
+        if (CheckCanUpgrade(type) == false) return -1;
+        return _toolDataList[(int)type].Upgrade_WoodCountList[_toolList[(int)type].UpgradeLevel + 1];
+    }
+
+    public int GetStoneCountToUpgrade(ToolType type)
+    {
+        if (CheckCanUpgrade(type) == false) return -1;
+        return _toolDataList[(int)type].Upgrade_StoneCountList[_toolList[(int)type].UpgradeLevel + 1];
+    }
+
+    public int GetCurrentLevel(ToolType type)
+    {
+        return GetTool(type).UpgradeLevel + 1;
+    }
+
     public void UpgradeTool(ToolType toolType)
     {
         ATool Tool = GetTool(toolType);
 
-        if (Tool.UpgradeLevel >= ToolManager.Instance.MaxUpgradeLevel) return;
+        if (CheckCanUpgrade(toolType) == false) return;
         // Ω∫≈» ¥√∏Æ±‚
         Tool.UpgradeLevel++;
-        Tool.Value += DataTable.Instance.GetToolData(StartTid + (int)toolType).Upgrade_AddValueList[Tool.UpgradeLevel];
+        Tool.Value += _toolDataList[(int)toolType].Upgrade_AddValueList[Tool.UpgradeLevel];
     }
 
     public void AddCurrentToolCount(ToolType toolType, int amount)
