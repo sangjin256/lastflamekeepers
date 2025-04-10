3760582396fbfdb0857805ec9352aa7a25a1ab84
pinehj <pinehj00@gmail.com>
2025-04-10 12:56:17 +0900

    Revert "Merge remote-tracking branch 'origin/WooBranch' into SHJ"
    This reverts commit 1f9b43b16149a3f5237644102e430f072f24b388, reversing
    changes made to 5eb6cf9af003d765ee3c30ce8c0887a4c60f89eb.

--- Assets/02.Scripts/UI/UIManager.cs
+++ Assets/02.Scripts/UI/UIManager.cs
@@ -1,8 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : BehaviourSingleton<UIManager>
 {
+    [Header("건물 생성 버튼")]
+    public List<Button> BuildingManageButtons;
+    public GameObject BuildingListPopup;
+
+    [Header("유닛 툴 버튼")]
+    public List<Button> UnitToolManageButtons;
+
+    private bool _canBuildStart;
     public GameObject FireObject;
+
+    public void SetCanBuildStart(bool canBuildStart)
+    {
+        _canBuildStart = canBuildStart;
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i <= (int)BuildingType.Forge; i++)
+        {
+            int index = i;
+            BuildingManageButtons[i].onClick.AddListener(() => BuildManager.Instance.StartBuildingMode(index));
+            BuildingManageButtons[i].onClick.AddListener(TryCloseBuildingListPopup);
+        }
+
+        _canBuildStart = false;
+    }
+
+    public void TryCloseBuildingListPopup()
+    {
+        if (!_canBuildStart)
+        {
+            return;
+        }
+
+        BuildingListPopup.SetActive(false);
+    }
 
     public void OnClickFire()
     {
