f63c2740c1c8df2377eef57ee5ef3913659b6761
pinehj <pinehj00@gmail.com>
2025-04-11 15:42:17 +0900

    [SHJ]능력치색변경코드

--- Assets/02.Scripts/UI/UI_UnitData.cs
+++ Assets/02.Scripts/UI/UI_UnitData.cs
@@ -27,23 +27,35 @@ public class UI_UnitData : MonoBehaviour
         UnitNameText.text = unitStat.Name;
 
         UnitMaxHealthText.text = unitStat.MaxHealth.AddedValue != 0
-            ? $"{unitStat.MaxHealth.BaisicValue}{unitStat.MaxHealth.AddedValue.ToString("(+#);(-#)")}"
-            : $"{unitStat.MaxHealth.BaisicValue}";
+            ? unitStat.MaxHealth.AddedValue > 0
+            ? $"{unitStat.MaxHealth.Value} <color=#57AC4B>({unitStat.MaxHealth.AddedValue})</color>"
+            : $"{unitStat.MaxHealth.Value} <color=#B13E38>({unitStat.MaxHealth.AddedValue})</color>"
+            : $"{unitStat.MaxHealth.Value}";
         UnitDamageText.text = unitStat.Damage.AddedValue != 0
-            ? $"{unitStat.Damage.BaisicValue}{unitStat.Damage.AddedValue.ToString("(+#);(-#)")}"
-            : $"{unitStat.Damage.BaisicValue}";
-        UnitAttackSpeedText.text = unitStat.AttackSpeed.AddedValue != 0 
-            ? $"{unitStat.AttackSpeed.BaisicValue}{unitStat.AttackSpeed.AddedValue.ToString("(+#);(-#)")}" 
-            : $"{unitStat.AttackSpeed.BaisicValue}";
-        UnitMoveSpeedText.text = unitStat.MoveSpeed.AddedValue != 0 
-            ? $"{unitStat.MoveSpeed.BaisicValue}{unitStat.MoveSpeed.AddedValue.ToString("(+#);(-#)")}" 
-            : $"{unitStat.MoveSpeed.BaisicValue  }";
-        UnitWoodSpeedText.text = unitStat.WoodSpeed.AddedValue != 0 
-            ? $"{unitStat.WoodSpeed.BaisicValue}{unitStat.WoodSpeed.AddedValue.ToString("(+#);(-#)")}" 
-            : $"{unitStat.WoodSpeed.BaisicValue}";
-        UnitRockSpeedText.text = unitStat.RockSpeed.AddedValue != 0 
-            ? $"{unitStat.RockSpeed.BaisicValue}{unitStat.RockSpeed.AddedValue.ToString("(+#);(-#)")}" 
-            : $"{unitStat.RockSpeed.BaisicValue}";
+            ? unitStat.Damage.AddedValue > 0
+            ? $"{unitStat.Damage.Value} <color=#57AC4B>({unitStat.Damage.AddedValue})</color>"
+            : $"{unitStat.Damage.Value} <color=#B13E38>({unitStat.Damage.AddedValue})</color>"
+            : $"{unitStat.Damage.Value}";
+        UnitAttackSpeedText.text = unitStat.AttackSpeed.AddedValue != 0
+            ? unitStat.AttackSpeed.AddedValue > 0
+            ? $"{unitStat.AttackSpeed.Value} <color=#57AC4B>({unitStat.AttackSpeed.AddedValue})</color>"
+            : $"{unitStat.AttackSpeed.Value} <color=#B13E38>({unitStat.AttackSpeed.AddedValue})</color>"
+            : $"{unitStat.AttackSpeed.Value}";
+        UnitMoveSpeedText.text = unitStat.MoveSpeed.AddedValue != 0
+            ? unitStat.MoveSpeed.AddedValue > 0
+            ? $"{unitStat.MoveSpeed.Value} <color=#57AC4B>({unitStat.MoveSpeed.AddedValue})</color>"
+            : $"{unitStat.MoveSpeed.Value} <color=#B13E38>({unitStat.MoveSpeed.AddedValue})</color>"
+            : $"{unitStat.MoveSpeed.Value}";
+        UnitWoodSpeedText.text = unitStat.WoodSpeed.AddedValue != 0
+            ? unitStat.WoodSpeed.AddedValue > 0
+            ? $"{unitStat.WoodSpeed.Value} <color=#57AC4B>({unitStat.WoodSpeed.AddedValue})</color>"
+            : $"{unitStat.WoodSpeed.Value} <color=#B13E38>({unitStat.WoodSpeed.AddedValue})</color>"
+            : $"{unitStat.WoodSpeed.Value}";
+        UnitRockSpeedText.text = unitStat.RockSpeed.AddedValue != 0
+            ? unitStat.RockSpeed.AddedValue > 0
+            ? $"{unitStat.RockSpeed.Value} <color=#57AC4B>({unitStat.RockSpeed.AddedValue})</color>"
+            : $"{unitStat.RockSpeed.Value} <color=#B13E38>({unitStat.RockSpeed.AddedValue})</color>"
+            : $"{unitStat.RockSpeed.Value}";
 
         _unitPositiveFeatureTextList = new List<TextMeshProUGUI>(UnitPositiveFeature.GetComponentsInChildren<TextMeshProUGUI>());
         _unitNegativeFeatureTextList = new List<TextMeshProUGUI>(UnitNegativeFeature.GetComponentsInChildren<TextMeshProUGUI>());
