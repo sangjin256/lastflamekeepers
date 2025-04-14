e99cb34316072c85c48e34fc90fbfcac807e60fe
SangJin Lee <56784814+sangjin256@users.noreply.github.com>
2025-04-14 09:40:52 +0900

    [LSJ] 파이어 매니저 수정

--- Assets/02.Scripts/Fire/FireManager.cs
+++ Assets/02.Scripts/Fire/FireManager.cs
@@ -189,7 +189,7 @@ public class FireManager : BehaviourSingleton<FireManager>
 
     public void ReduceFire()
     {
-        if (CurrentRange - 1 <= MinRangeData)
+        if (CurrentRange - 1 < MinRangeData)
         {
             if(CurrentRange - 1 <= MinRangeDeath)
             {
@@ -205,11 +205,20 @@ public class FireManager : BehaviourSingleton<FireManager>
                 NextFirePercent = DataTable.Instance.GetFireLightData(StartTID).NextFirePercent;
             }
         }
+        else if(CurrentRange - 1 == MinRangeData)
+        {
+            CurrentRange--;
+            CurrentFirePercent = 1;
+            CurrentWood = 0;
+            DOTween.To(() => _fireLight.size, x => _fireLight.size = x, CurrentRange, 0.5f).SetEase(Ease.OutCubic);
+            WoodToLvUp = DataTable.Instance.GetFireLightData(StartTID).WoodAmout;
+            NextFirePercent = DataTable.Instance.GetFireLightData(StartTID).NextFirePercent;
+        }
         else
         {
-            CurrentFirePercent--;
             CurrentWood = 0;
             CurrentRange--;
+            CurrentFirePercent = DataTable.instance.GetFireLightData(StartTID + CurrentRange - MinRangeData - 1).NextFirePercent;
             DOTween.To(() => _fireLight.size, x => _fireLight.size = x, CurrentRange, 0.5f).SetEase(Ease.OutCubic); 
             WoodToLvUp = DataTable.Instance.GetFireLightData(StartTID + CurrentRange - MinRangeData).WoodAmout;
             NextFirePercent = DataTable.Instance.GetFireLightData(StartTID + CurrentRange - MinRangeData).NextFirePercent;
